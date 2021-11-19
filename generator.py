2c77f6d170036a77fdd857608849f04c83c86f81
shaagnik <shaagnik.m@gmail.com>
2021-11-19 12:55:19 -0800

    generate piece

--- generator.py
+++ generator.py
@@ -3,20 +3,36 @@
 from music21 import *
 from random import randint
 
-
+class Voice:
+    def __init__(self):
+        self.notes = []
+    def setNotes(self, notes):
+        self.notes = notes
+    def play(self):
+        x = stream.Stream(self.notes)
+        s = midi.realtime.StreamPlayer(x)
+        s.play()
 
 def gen_voice(length, lowest, highest):
-    result = stream.Stream()
     tone_range = interval.Interval(lowest, highest)
+    notes = []
     for l in range(0, length + 1):
         dist = randint(0, tone_range.cents/100)
-        x = lowest.transpose(dist)
-        result.append(x)
-    s = midi.realtime.StreamPlayer(result)
-    s.play()
+        newNote = lowest.transpose(dist)
+        notes.append(newNote)
+    result = Voice()
+    result.setNotes(notes)
     return result
 
-n = note.Note('c3')
-n2 = note.Note('c4')
-x = gen_voice(16, n, n2)
-
+class Piece:
+    def __init__(self):
+        self.voices = []
+    def setVoices(self, voices):
+        self.voices = voices
+    def play(self):
+        x = stream.Stream()
+        for v in self.voices:
+            y = stream.Part(v.notes)
+            x.append(y)
+        s = midi.realtime.StreamPlayer(x)
+        s.play()
